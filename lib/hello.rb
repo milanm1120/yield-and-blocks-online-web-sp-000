f348dd480f2d599f4fe84a7601feb40cef6954ad
Milan Mody <milanm1120@gmail.com>
2021-02-22 23:30:20 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -15,7 +15,7 @@ end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Thomas", "Tyler", "Tony" "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
